73edd7d250deddda8a362cdfb946a2a58d934d24
Kirsten C <cartekir@oregonstate.edu>
2019-11-21 21:17:55 -0800

    Slider not working, nav working on all pages, pictures in grid

--- .vs/script.js
+++ .vs/script.js
@@ -1,30 +1,17 @@
 // JavaScript source code
 
 //adapted from https://www.w3schools.com/howto/howto_js_slideshow.asp see automatic slideshow
-var sliderIndex = 1;
-showSlide(sliderIndex);
+var slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-    showSlide(sliderIndex += n);
-}
-
-function curSlide(n) {
-    showSlide(slideIndex = n);
-}
-
-function showSlide(n) {
+function showSlides() {
     var i;
-    var slides = document.getElementByClassName("mainSlider");
-    var dot = document.getElementsByClassName("dot");
-
+    var slides = document.getElementsByClassName("mainSlider");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+        slides[i].style.display = "none";
     }
     slideIndex++;
-
-    if (slideINdex > slides.length)
-    { slideIndex = 1 }
-
+    if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlide, 15000);
-}
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
